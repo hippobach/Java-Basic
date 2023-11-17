2716617dbaa7261e24a0ff7c7c7080e53c87b50b
Bach Nguyen <nguyenxuanbach03nd@gmail.com>
2023-11-17 22:31:16 +0700

    data type in java

---
+++ src/Bai4.java
@@ -0,0 +1,9 @@
+public class Bai4 {
+    public static void main(String[] args) {
+//        Kiểu Int
+        System.out.println("Min của kiểu dữ liệu interger: " +Integer.MIN_VALUE);
+        System.out.println("Max của kiểu dữ liệu interger: " +Integer.MAX_VALUE);
+//        Kiểu Byte
+        System.out.println("Max của kiểu byte: "+ Byte.MAX_VALUE);
+    }
+}
