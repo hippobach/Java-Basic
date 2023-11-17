208259811b802e70144e96e6352af8ad1e46328c
Bach Nguyen <nguyenxuanbach03nd@gmail.com>
2023-11-17 22:07:42 +0700

    comment in java

---
+++ src/Bai3.java
@@ -0,0 +1,9 @@
+public class Bai3 {
+    public static void main(String[] args) {
+        // day la ghi tren mot dong
+        System.out.println("linr 1");
+        System.out.println("linr 2");
+        /*System.out.println("linr 3");
+        System.out.println("line 4");*/
+    }
+}
