49a0e35c5c9338cd4f22fd44a9bf6234f5d3c38a
Bach Nguyen <nguyenxuanbach03nd@gmail.com>
2023-11-18 15:27:36 +0700

    operators in java

---
+++ src/Bai07.java
@@ -0,0 +1,19 @@
+public class Bai07 {
+    public static void main(String[] args) {
+        int a = 11;
+        int b = 5;
+        System.out.println("a = " +a);
+        System.out.println("b = " +b);
+
+//        + operator
+        System.out.println("a+b=" + (a+b));
+//        - operator
+        System.out.println("a-b=" + (a-b));
+//        * operator
+        System.out.println("a*b=" + (a*b));
+//        / operator  => casting type
+        System.out.println("a/b=" + ((double)a/b));
+//        mod operator
+        System.out.println("a%b=" + (a%b));
+    }
+}
