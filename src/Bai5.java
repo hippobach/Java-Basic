4f7cdd961c42616c87724d29863703d88b36126a
Bach Nguyen <nguyenxuanbach03nd@gmail.com>
2023-11-18 15:05:00 +0700

    data type in Java

---
+++ src/Bai5.java
@@ -0,0 +1,16 @@
+public class Bai5 {
+    public static void main(String[] args) {
+//        Khai báo biến
+        int a;
+        byte tuoi;
+        float diemToan;
+//        Khai báo nhiều biến cùng lúc và cùng kiểu
+        int c,d,e,f;
+//        Khởi tạo biến
+        int tuoiCon = 10;
+        float diemVan = 7.5f;
+        double diemAnh = 9.5;
+//        Hằng số
+        final int DO_SOI = 100;
+    }
+}
