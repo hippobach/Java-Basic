cbc3de034ac97074458bfc9a74e3095ec885b735
Bach Nguyen <nguyenxuanbach03nd@gmail.com>
2023-11-18 15:48:35 +0700

    comparision in java

--- src/Bai7.java
+++ src/Bai7.java
@@ -1,4 +1,4 @@
-public class Bai07 {
+public class Bai7 {
     public static void main(String[] args) {
         int a = 11;
         int b = 5;
---
+++ src/Bai8.java
@@ -0,0 +1,12 @@
+public class Bai8 {
+    public static void main(String[] args) {
+//        gan cong
+        int x = 8;
+        x+=5;
+        System.out.println("x=" +x);
+//        gan tru
+        int y=8;
+        y-=5;
+        System.out.println("y=" +y);
+    }
+}
