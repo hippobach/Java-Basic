9e332b56fcb10ec2961eccb0bf329adadaac3761
Bach Nguyen <nguyenxuanbach03nd@gmail.com>
2023-12-25 21:23:35 +0700

    while true and do while loop in java

---
+++ src/Bai15.java
@@ -0,0 +1,21 @@
+public class Bai15 {
+    public static void main(String[] args) {
+//        tinh tong cac so tu 1 toi 5
+        int a = 1;
+        int tong = 0;
+        do {
+            tong += a;
+            a++;
+        } while (a<=5);
+        System.out.println("Tong bang: "+ tong);
+
+//        int n = 0;
+//        while (true) {
+//            n++;
+//            System.out.println("n = " +n);
+//            if(n == 5) {
+//                break;
+//            }
+//        }
+    }
+}
